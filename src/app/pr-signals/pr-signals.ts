e4a860030ff720ef00a3c121591b73f0191bec98
uditparmar1122@outlook.com <uditparmar1122@outlook.com>
2025-12-01 00:33:04 +0530

    set & update  data and style using Signals

--- src/app/pr-signals/pr-signals.ts
+++ src/app/pr-signals/pr-signals.ts
@@ -1,4 +1,4 @@
-import { Component,signal } from '@angular/core';
+import { Component,signal,effect } from '@angular/core';
 
 @Component({
   selector: 'app-pr-signals',
@@ -7,15 +7,33 @@ import { Component,signal } from '@angular/core';
   styleUrl: './pr-signals.css',
 })
 export class PrSignals {
+  constructor() {
+    effect(()=>{
+    console.log("conuter updated :"+this.count());
+    console.log("color updated :"+this.color());
+
+  })
+  }
   count = signal(0);
   color = signal('color:red;');
+  bg=signal('background-color:white;');
   increment() {
-    this.count.set(this.count() + 1); 
-    this.color.set('color:blue;');
+    this.count.set(this.count() + 1);
+    this.checkColor(this.count());
   }
   decrement() {
     this.count.update(()=>this.count() - 1);
-    this.color.set('color:gray;');
+    this.checkColor(this.count());
   }
-
+  checkColor(countval:number){
+    if(countval>0){
+      this.color.set('color:blue;');
+      this.bg.set('background-color:orange;');
+    }else if(countval<0){
+      this.color.set('color:gray;');
+    } 
+    else{
+      this.color.set('color:orange;');
+    }
+  }
 }
